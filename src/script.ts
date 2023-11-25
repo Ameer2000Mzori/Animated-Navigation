9c675e26edaf961ad2863a314f9ae2eb9873b691
Ameer <ameerapex496@gmail.com>
2023-11-25 18:40:11 +0100

    navbar func finished

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,18 @@
-console.log("hallo world");
+// selecting our elements
+const iEl = document.querySelector("i") as HTMLElement;
+const navLinks = document.getElementsByClassName("nav-Links")[0];
+
+// our functions
+const navHandler = () => {
+  if (navLinks.classList.contains("active")) {
+    navLinks.classList.remove("active");
+    iEl.classList.replace("fa-bars", "fa-xmark");
+  } else {
+    navLinks.classList.add("active");
+    iEl.classList.replace("fa-xmark", "fa-bars");
+  }
+};
+// <i class="fa-solid fa-xmark"></i>
+
+// our eventlsinters
+iEl.addEventListener("click", navHandler);
